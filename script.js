f5d2e334bf6c2090b6317c818dd7fec30ab18898
jose <josesaa0324@gmail.com>
2024-10-20 21:08:12 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+let num1 = parseInt(prompt
+    ("prenda"))
+    
+let num2 = parseInt(prompt
+        ("descuento"))  
+    
+    
+    let descuento = num1 * num2 /100
+    let total= num1 -descuento
+    
+    //mostrar resultado//
+    document.write(total,"<br>")
+    
+    
